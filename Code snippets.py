66454d1a8dae87c068126e0a7591fec2340644d2
p_cohen <peter.cohen.uchicago@gmail.com>
2015-09-03 09:50:02 -0500

    started tracking code I like

---
+++ Code snippets.py
@@ -0,0 +1,124 @@
+def create_val_and_train(df, seed, ids, split_rt = .20):
+    """
+        Creates two samples (generally used to create
+        train and validation samples)
+
+        Parameters
+        ----------------------------------------------------
+        ids: this is the level of randomization, so if you want to
+        randomize countries, rather than cities, you would
+        set this to 'countries'
+
+        split_rate: pct of data to assign as validation
+
+        Output
+        ----------------------------------------------------
+        trn_for_mods (1-split_rate of df), trn_for_val (split_rate of data)
+
+
+    """
+    np.random.seed(seed)
+    # Get vector of de-dupped values of ids
+    id_dat = pd.DataFrame(df[ids].drop_duplicates())
+    # Create random vector to split train val on
+    vect_len = len(id_dat.ix[:, 0])
+    id_dat['rand_vals'] = (np.array(np.random.rand(vect_len,1)))
+    df = pd.merge(df, id_dat, on=ids)
+    # split data into two dfs
+    trn_for_mods = df[df.rand_vals > split_rt]
+    trn_for_val = df[df.rand_vals <= split_rt]
+    # drop rand_vals
+    trn_for_val = trn_for_val.drop('rand_vals', axis=1)
+    trn_for_mods = trn_for_mods.drop('rand_vals', axis=1)
+    return trn_for_mods, trn_for_val
+
+def rmsle(actual, predicted):
+    """
+    #### __author__ = 'benhamner'
+    Computes the root mean squared log error.
+    This function computes the root mean squared log error between two lists
+    of numbers.
+    Parameters
+    ----------
+    actual : list of numbers, numpy array
+             The ground truth value
+    predicted : same type as actual
+                The predicted value
+    Returns
+    -------
+    score : double
+            The root mean squared log error between actual and predicted
+    """
+    sle_val = (np.power(np.log(np.array(actual)+1) -
+               np.log(np.array(predicted)+1), 2))
+    msle_val = np.mean(sle_val)
+    return np.sqrt(msle_val)
+
+def gen_weights(df):
+    """
+    This creates weights based on the number of rows per tube assembly
+
+    :param df: data frame to add weights to
+    :return: dataframe with wieghts
+    """
+    df['one'] = 1
+    grouped = df.groupby('tube_assembly_id')
+    counts = grouped.one.sum().reset_index()
+    counts = counts.rename(columns={'one': 'ob_weight'})
+    counts['ob_weight'] = counts['ob_weight']
+    df = df.merge(counts, on='tube_assembly_id')
+    df = df.drop('one', axis=1)
+    return df
+
+def create_feat_list(df, non_features):
+    feats = list(df.columns.values)
+    for var in non_features:
+        feats.remove(var)
+    return feats
+
+def predict_all_data(dfs, mod, name, features):
+    for df in dfs:
+        df = write_preds(df, mod, name, features)
+
+def subm_correl(subm1, subm2, id, target):
+    """
+    Measures correlation between to Kaggle submissions
+    """
+    subm1 = pd.read_csv(SUBM_PATH + subm1)
+    subm2 = pd.read_csv(SUBM_PATH + subm2)
+    subm2 = subm2.rename(columns={target: 'target2'})
+    merged_df = subm1.merge(subm2, on=id)
+    return merged_df.corr()
+
+def merge_subms(subm_dict, path, name, target):
+    """
+    :param subm_dict: Dictionary of dfs to merge, where key is csv name and
+    value is weight (values must sum to 1 to keep outcome in original range
+    :param path: path to submission folder
+    :param name: name of new file
+    :param target: outcome variable of submission
+    :return:
+    """
+    subm = pd.read_csv(path+'template.csv')
+    for csv, weight in subm_dict.iteritems():
+        # Read in a new csv
+        score = pd.read_csv(path+csv)
+        # rename target to avoid merge issues
+        score = score.rename(columns={target: 'target2'})
+        # Merge files to be averaged
+        subm = subm.merge(score, on='id')
+        subm[target] += weight * subm['target2']
+        subm = subm.drop('target2', 1)
+    subm.to_csv(path+name, index=False)
+
+def check_weight_and_merge(dict, name):
+    """
+    :param dict: file, weight pairs
+    :param name: name of resulting blended file
+    :return: blended file saved to server
+    """
+    total_weight = 0
+    for key, val in dict.iteritems():
+        total_weight += val
+    print "The total weight should be 1.0, it is: %s" % (total_weight)
+    merge_subms(dict, SUBM_PATH, name, 'cost')
